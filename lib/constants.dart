71c0bdee8e0e0282de3476bf5b25d258d4eb32e6
SaberaKarimi <saberakarimi85@gmail.com>
2023-02-18 12:23:17 +0430

    challenge coin tracker

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,3 @@
 //TODO: 1.3.3 Put your API key and CoinAPI's base url in the following constants
-const kAPIKey = '';
-const kBaseURL = '';
+const kAPIKey = '9319625E-4856-4485-9C49-3094D224937E';
+const kBaseURL = 'https://rest.coinapi.io/v1/exchangerate/';
